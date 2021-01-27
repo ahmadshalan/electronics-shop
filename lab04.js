f5a6aabff9cdc0f15b050c4d3d63a1e9a8f8c7f0
ahmad shalan <engashalan@gmail.com>
2021-01-27 14:41:31 +0200

    lab04a addition

---
+++ lab04.js
@@ -0,0 +1,18 @@
+console.log('hello');
+console.log('lab04');
+console.log('ahamd shalan');
+console.log(2+5);
+
+var message = '';
+var age = prompt('what is your age');
+console.log(age);
+
+if (age>18){
+   message = 'welcome'
+
+}else{ message = 'not allowed'
+}
+
+document.write('<h3>' + message + age + '</h3>');
+
+
