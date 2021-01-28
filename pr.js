bd6b6b7e54fa6f2a51a089847540f655de964298
ahmad shalan <engashalan@gmail.com>
2021-01-28 15:57:39 +0200

    lab05

--- pr.js
+++ pr.js
@@ -16,3 +16,37 @@ if (age>18){
 
 document.write('<h3>' + message + age + '</h3>');
 
+
+
+
+var product = prompt('what product type do you want?');
+
+
+
+while(product !== 'phone' && product !== 'tv' && product !== 'laptop'){
+   product =prompt('please write valid product ')
+}
+
+
+
+if (product=== 'phone'){
+   product = '<img src="images/phone.jpg"/>';
+}else if(product==='laptop'){
+   product = '<img src="images/laptop.jpg"/>';
+
+}else if(product==='tv'){
+   product = '<img src="images/tv.jpg"/>';
+}
+
+var imgNum = prompt('how many pic you want to see')
+for( var i = 0 ; i<imgNum; i++){
+
+
+   console.log(i);
+
+   document.write(product);
+}
+
+
+
+
